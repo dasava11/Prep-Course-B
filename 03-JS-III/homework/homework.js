e01535fb7ce73f8ab74a107fbf0e849e59806fad
David Sanchez <dasava11@outlook.com>
2022-12-06 17:26:39 -0500

    cargando avances

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,29 +8,71 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+
+    var n = 1
+    var suma = 0
+    while (n <= 10) {
+      suma = suma + n; 
+      n = n + 1; 
+    }
+    return suma; 
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+
+  // var ArrayPares = []
+
+  // for(let i=0; i<array.length; i++) {
+  //   if(array[i]%2 === 0)
+  //   ArrayPares.push(array[i]);
+  // }
+  // return ArrayPares;
+
+  var pares = array.filter((elemento) => elemento%2 === 0);
+  return pares;
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+
+
+  // var ArrayPotencia = []
+  // for(let i=0; i<array.length; i++) {
+  //   ArrayPotencia.push(array[i]*array[i])
+  // }
+  // return ArrayPotencia;
+
+  var potencia = array.map((num) => num ** 2);
+  return potencia; 
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+
+
+  // var suma = 0
+  // for(let i=0; i<array.length; i++) {
+  //   suma = suma + array[i]
+  // }
+  // return suma;
+
+  var suma = array.reduce((acumulador, numero) => acumulador + numero); 
+  return suma;
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  var n = num.toString();
+  return n.length;
+
 }
   
   
